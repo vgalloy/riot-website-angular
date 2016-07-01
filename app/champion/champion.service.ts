c3985ce1a35a1119d5813557bab2c223ef62d1cd
nbelleme <nbelleme@excilys.com>
2016-07-01 14:05:17 +0200

    [Clean] cleaning git repo

--- app/champion/champion.service.ts
+++ app/champion/champion.service.ts
@@ -1,1 +1,24 @@
-export class ChampionService {}
+import {Http} from "@angular/http";
+import {Champion} from "./champion";
+import "rxjs/add/operator/toPromise";
+
+export class ChampionService {
+
+    private heroesUrl = "bonjour";
+    constructor(private http:Http) {
+    }
+
+    getHeroes():Promise<Champion> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data)
+            .catch(this.handleError);
+    }
+
+    handleError(){
+        //TODO
+        console.log("Error");
+    }
+
+
+}
