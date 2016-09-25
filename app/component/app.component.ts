4a77e91d01fd7d6cbcb44e106d1551de445ef7d2
Vincent Galloy <vgalloy@excilys.com>
2016-09-25 21:33:01 +0200

    [ ADD ] : New title

--- app/component/app.component.ts
+++ app/component/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
     styleUrls: ['app/css/app.component.css']
 })
 export class AppComponent {
-    title = 'Riot Angular WS';
+    title = '3CS minutes V 1.1';
 }
