e8544b09ebb47945447eef5df357f24a15724cf9
Gilberto Rasamoela <gilberto.rasamoela@gmail.com>
2016-09-25 11:46:29 +0200

    fix missing files

---
+++ app/component/game-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { GameService } from "../service/game.service";
+import { LastGame } from ".././model/last-game";
+import { SummonerService } from "../service/summoner.service";
+import { Region } from "../model/region";
+import { Summoner } from "../model/summoner";
+import { PlayerTimeline } from "../model/player-timeline";
+
+// Will be used for the right part of the project, along with the map-- Actually unused
+@Component({
+    selector: 'game-details',
+    templateUrl: 'app/html/game-details.component.html',
+    styleUrls: ['app/css/game-detail.component.css']
+})
+export class GameDetailsComponent implements OnInit {
+    game:LastGame;
+    name:string = "Summoner Name";
+    timelines:PlayerTimeline[];
+    constructor(private route:ActivatedRoute,
+                private gameService:GameService,
+                private router:Router,
+                private summonerService:SummonerService) {
+    }
+
+    ngOnInit():void {
+        this.route.params.forEach((params:Params) => {
+            let summonerId = params['summonerId'];
+            let regionString:string = params['region'];
+            let region = Region[regionString];
+            let gameId = params['gameId'];
+            console.log(params);
+            console.log(gameId);
+            this.summonerService.getSummonerById(region, summonerId)
+                .then(summoner => this.name = summoner.name);
+
+            //to only do when gameId is defined, when a game is visible
+            if(gameId)    
+            this.gameService.getTimeline(region, gameId)
+                .then(timelines => this.timelines = timelines);
+        });
+    }
+
+}
