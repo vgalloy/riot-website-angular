c96a384876fd16e33834bacaeb1cf2754402078c
Vincent Galloy <vgalloy@excilys.com>
2016-12-11 20:14:00 +0100

    [ NEW ] : Hash location strategy

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,19 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {routing} from "./app.routing";
-import {AppComponent} from "./component/app.component";
-import {SummonerComponent} from "./component/summoner.component";
-import {GameListComponent} from "./component/game-list.component";
-import {GameDetailsComponent} from "./component/game-details.component";
-import {ChampionComponent} from "./component/champion.component";
-import {SummonerService} from "./service/summoner.service";
-import {GameService} from "./service/game.service";
-import {ChampionService} from "./service/champion.service";
-import {GameSummaryComponent} from "./component/game-summary.component";
-import {CachedGameService} from "./service/cached-game.service";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { routing } from "./app.routing";
+import { AppComponent } from "./component/app.component";
+import { SummonerComponent } from "./component/summoner.component";
+import { GameListComponent } from "./component/game-list.component";
+import { GameDetailsComponent } from "./component/game-details.component";
+import { ChampionComponent } from "./component/champion.component";
+import { SummonerService } from "./service/summoner.service";
+import { GameService } from "./service/game.service";
+import { ChampionService } from "./service/champion.service";
+import { GameSummaryComponent } from "./component/game-summary.component";
+import { CachedGameService } from "./service/cached-game.service";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 @NgModule({
     imports: [
@@ -30,6 +31,7 @@ import {CachedGameService} from "./service/cached-game.service";
         ChampionComponent
     ],
     providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy},
         GameService,
         CachedGameService,
         SummonerService,
